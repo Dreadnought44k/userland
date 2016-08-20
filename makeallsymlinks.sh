ec5d1375682364bc0ba5a73ddca6f64d1461b059
maxice8 <thinkabit.ukim@gmail.com>
2016-08-20 06:16:45 -0300

    Add makeallsymlinks script
    read the info lists and symlinks the programs, effectively changing the userland
    
    used in dash shell and not tested with others.

---
+++ makeallsymlinks.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Scripts that make all symlinks for my userland
+# Assumes that GNU coreutils exist to fill gaps that these don't provide
+# Assumes the existance of info files {busyboxTools,sbase-boxTools,ubase-boxTools}
+# Doesn't replace functions that are vital for Gentoo, again, expect GNU Coreutils
+
+grep -v '^ *#' < busyboxTools | while IFS= read -r line
+do
+    ln -s /bin/busybox "$line"
+done
+
+grep -v '^ *#' < sbase-boxTools | while IFS= read -r line
+do
+    ln -s /bin/sbase-box "$line"
+done
+
+grep -v '^ *#' < ubase-boxTools | while IFS= read -r line
+do 
+    ln -s /bin/ubase-box bbdir/"$line"
+done
