1343488987bf8306f73b673da4b6234a5d5a87f9
Dreadnought44k <thinkabit.ukim@gmail.com>
2016-08-24 14:36:28 -0300

    Remove blocking comment

--- updateinfo.sh
+++ updateinfo.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 TOOLKIT=busybox
-find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" #> "$TOOLKIT"Tools
+find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" > "$TOOLKIT"Tools
 
 TOOLKIT=toybox
-find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" #> "$TOOLKIT"Tools
+find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" > "$TOOLKIT"Tools
 
 TOOLKIT=sbase-box
-find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" #> "$TOOLKIT"Tools
+find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" > "$TOOLKIT"Tools
 
 TOOLKIT=ubase-box
-find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" #> "$TOOLKIT"Tools
+find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" > "$TOOLKIT"Tools
 
