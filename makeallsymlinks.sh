a29220230aaf9fe5fa290060c6a66e92c23cb13a
Dreadnought44k <thinkabit.ukim@gmail.com>
2016-09-02 15:28:38 -0300

    Repurpse makeallsymlinks to just call all other scripts

--- makeallsymlinks.sh
+++ makeallsymlinks.sh
@@ -4,22 +4,4 @@
 # Assumes the existance of info files {busyboxTools,sbase-boxTools,ubase-boxTools}
 # Doesn't replace functions that are vital for Gentoo, again, expect GNU Coreutils
 
-grep -v '^ *#' < busyboxTools | while IFS= read -r line
-do
-    ln -sf /bin/busybox "$line"
-done
-
-grep -v '^ *#' < sbase-boxTools | while IFS= read -r line
-do
-    ln -sf /bin/sbase-box "$line"
-done
-
-grep -v '^ *#' < ubase-boxTools | while IFS= read -r line
-do 
-    ln -sf /bin/ubase-box "$line"
-done
-
-# grep -v '^ *#' < toyboxTools | while IFS= read -r line
-# do
-#     ln -sf /bin/toybox "$line"
-# done
+/bin/sh make{busybox,toybox,sbase-box,ubase-box}symlinks.sh
