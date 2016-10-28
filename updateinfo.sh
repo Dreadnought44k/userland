b7e79c1c6db7b13c3cb8336882bd5f75a76bb218
maxice8 <thinkabit.ukim@gmail.com>
2016-10-28 16:41:08 -0200

    Update with more places

--- updateinfo.sh
+++ updateinfo.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 TOOLKIT=busybox
-find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" > "$TOOLKIT"Tools
+find /usr/sbin /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" > "$TOOLKIT"Tools
 
 TOOLKIT=toybox
-find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" > "$TOOLKIT"Tools
+find /sbin /bin /usr/sbin /usr/bin -lname /usr/bin/"$TOOLKIT" > "$TOOLKIT"Tools
 
 TOOLKIT=sbase-box
-find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" > "$TOOLKIT"Tools
+find /sbin /bin /usr/bin /usr/sbin -lname /bin/"$TOOLKIT" > "$TOOLKIT"Tools
 
 TOOLKIT=ubase-box
-find /sbin /bin /usr/bin -lname /bin/"$TOOLKIT" > "$TOOLKIT"Tools
+find /sbin /bin /usr/sbin /usr/bin -lname /bin/"$TOOLKIT" > "$TOOLKIT"Tools
 
